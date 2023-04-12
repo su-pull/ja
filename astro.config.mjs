2dabdc0fe3a91860a7461361b653d89d7a20f707
S.Watanabe <ocelot2828@gmail.com>
2023-04-12 22:30:33 +0900

    chore(ssr false): add

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,8 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	buildOptions: {
+		output: "dist",
+		ssr: false,
+	  },	
 });
