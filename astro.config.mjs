2cbc155039a8595fada8caee5ddbb8d0d55e2c71
S.Watanabe <ocelot2828@gmail.com>
2023-04-12 22:34:23 +0900

    chore(fix): up

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
-	buildOptions: {
-		output: "dist",
-		ssr: false,
-	  },	
+	output: "dist",
+	ssr: false
 });
