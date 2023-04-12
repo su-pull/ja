45328a70465d4a7a5050633243ea260c10fe62cb
S.Watanabe <ocelot2828@gmail.com>
2023-04-12 22:45:58 +0900

    feat(prefetch): add

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
-
+import prefetch from '@astrojs/prefetch';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	output: "dist",
+	integrations: [mdx(), sitemap(), prefetch()],
 	ssr: false
 });
