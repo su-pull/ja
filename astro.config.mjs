ec20e185d488338d527b8839fcc742cd3575c98b
sou <ocelot2828@gmail.com>
2023-05-20 15:30:25 +0900

    chore(*fix): refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+import prefetch from '@astrojs/prefetch'
 
-import sitemap from '@astrojs/sitemap';
-import prefetch from '@astrojs/prefetch';
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), prefetch()],
-	ssr: false
-});
+  site: 'https://example.com/',
+  integrations: [sitemap(), prefetch()]
+})
