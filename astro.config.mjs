e0d0bc77d87fdbb4f04580a39a0e0e0dc81756f1
S.Watanabe <ocelot2828@gmail.com>
2023-05-21 23:39:50 +0900

    feat(@astrojs/react): integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import prefetch from '@astrojs/prefetch'
 
 export default defineConfig({
   site: 'https://example.com/',
-  integrations: [sitemap(), prefetch()]
+  integrations: [react(), sitemap(), prefetch()]
 })
