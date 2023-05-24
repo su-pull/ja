7c4c22b324176d31ae3082eb7f2cdffd994d1bc0
S.Watanabe <ocelot2828@gmail.com>
2023-05-24 17:21:59 +0900

    feat(shiki image): add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
+import image from '@astrojs/image'
 import sitemap from '@astrojs/sitemap'
 import prefetch from '@astrojs/prefetch'
 
 export default defineConfig({
   site: 'https://example.com/',
-  integrations: [react(), sitemap(), prefetch()]
+  integrations: [react(), sitemap(), prefetch(), image()],
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark'
+    }
+  }
 })
